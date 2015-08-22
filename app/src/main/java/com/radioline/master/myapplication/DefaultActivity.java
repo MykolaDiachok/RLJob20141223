a76d88406649b3f218fbcf2c2cce610c539fce7e
Nickolay Dyachok <ndyachok@gmail.com>
2015-08-22 05:04:04 +0300

    add materialDrawer

---
+++ app/src/main/java/com/radioline/master/myapplication/DefaultActivity.java
@@ -0,0 +1,13 @@
+package com.radioline.master.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.radioline.master.myapplication.R;
+
+public class DefaultActivity extends AppCompatActivity {
+
+
+}
